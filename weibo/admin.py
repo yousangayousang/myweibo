f929c2b2835bdcc0739ae6f01549c1b4c6447bd6
yousangayousang <2586324057@qq.com>
2018-01-04 21:19:04 +0800

    修改admin系统

--- weibo/admin.py
+++ weibo/admin.py
@@ -3,4 +3,14 @@ from .models import Article,Suggest,Review
 
 # Register your models here.
 
-admin.site.register([Article,Suggest,Review])
+@admin.register(Article)
+class ArticleAdmin(admin.ModelAdmin):
+	list_display=('id','title','created_time')
+
+@admin.register(Suggest)
+class SuggestAdmin(admin.ModelAdmin):
+	list_display=('name','Suggest_email','suggest_time')
+
+@admin.register(Review)
+class ReviewAdmin(admin.ModelAdmin):
+	list_display=('user_name','user','review_time')
